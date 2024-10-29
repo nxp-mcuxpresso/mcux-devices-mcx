f6a80d5012389b4b23777e19137a707a524b253c
Feng Wei <wei.feng@nxp.com>
2024-10-29 13:37:03 +0800

    MCUXpresso SDK 24.12.00 pvw2 release update
    Signed-off-by: Feng Wei <wei.feng@nxp.com>

---
+++ shared.cmake
@@ -0,0 +1,6 @@
+
+# project segment: common configuration
+include(${SdkRootDirPath}/arch/arm/configuration/mcx.cmake)
+
+# middlewares and components
+include(${SdkRootDirPath}/devices/arm/shared.cmake)

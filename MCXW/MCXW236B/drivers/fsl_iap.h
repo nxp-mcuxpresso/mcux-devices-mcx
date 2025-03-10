8ffce5f332cbc8c8e4e2e565ede63fcf0878d9b3
Allen Zhang <chunfeng.zhang@nxp.com>
2025-03-10 16:06:50 +0800

    [MCUX-77324] Update the license of the MCXW23 header file
    - Shall use the BSD-3-Clause
    
    Signed-off-by: AllenZhang <chunfeng.zhang@nxp.com>

--- MCXW/MCXW236B/drivers/fsl_iap.h
+++ MCXW/MCXW236B/drivers/fsl_iap.h
@@ -1,15 +1,8 @@
 /*
- * Copyright 2018-2022, 2024 NXP
- * NXP Confidential and Proprietary.
- * This software is owned or controlled by NXP and may only be used
- * strictly in accordance with the applicable license terms. By expressly
- * accepting such terms or by downloading, installing, activating and/or
- * otherwise using the software, you are agreeing that you have read, and
- * that you agree to comply with and are bound by, such license terms. If
- * you do not agree to be bound by the applicable license terms, then you
- * may not retain, install, activate or otherwise use the software.
+ * Copyright 2024-2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
  */
-
 #ifndef __FSL_IAP_H_
 #define __FSL_IAP_H_
 

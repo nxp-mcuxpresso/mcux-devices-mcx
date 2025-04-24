d1a0cf7fc413051133a3642360398fe058ff9e4c
Holt.Sun <holt.sun@nxp.com>
2025-04-24 17:23:31 +0800

    MCUX-77571 Fix power driver issues.
    1. Fix power driver name in CMakeLists_power.txt
    2. Update POWER_EnterStandbyMode and POWER_RequestResetMode as common function instead of static inline.
    
    Signed-off-by: Holt.Sun <holt.sun@nxp.com>

--- MCXE/MCXE31B/drivers/fsl_power.c
+++ MCXE/MCXE31B/drivers/fsl_power.c
@@ -19,7 +19,8 @@ static power_pmc_callback_t s_powerPmcCallback;
 /*!
  * brief Gets the POWER PMC version id.
  *
- * This function gets the PMC version id including whether the PMC supports last mile regulator.
+ * This function gets the PMC version id including whether the PMC supports last
+ * mile regulator.
  *
  * param versionId         Pointer to pmc_version_id_t structure.
  */
@@ -36,6 +37,44 @@ void POWER_GetPMCVersionID(pmc_version_id_t *versionId)
     else
     {
         versionId->supportLastMileRegulator = false;
+    }
+}
+
+/*!
+ * brief Request to enter standby mode.
+ */
+void POWER_EnterStandbyMode(void)
+{
+    MC_ME->MAIN_COREID = MC_ME_MAIN_COREID_CIDX(0U) | MC_ME_MAIN_COREID_PIDX(0U);
+
+    MC_ME->MODE_CONF = MC_ME_MODE_CONF_STANDBY_MASK;
+    MC_ME->MODE_UPD  = MC_ME_MODE_UPD_MODE_UPD_MASK;
+
+    /*! Write the first key */
+    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0x5AF0U);
+    /*! Write the inverted key */
+    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0xA50FU);
+
+    __ASM volatile("wfi");
+}
+
+/*!
+ * brief Request destructive or functional reset mode transition.
+ *
+ * param resetType Reset type, see ref mc_me_reset_type_t.
+ */
+void POWER_RequestResetMode(mc_me_reset_type_t resetType)
+{
+    MC_ME->MODE_CONF = (uint32_t)resetType;
+    MC_ME->MODE_UPD  = MC_ME_MODE_UPD_MODE_UPD_MASK;
+
+    /*! Write the first key */
+    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0x5AF0U);
+    /*! Write the inverted key */
+    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0xA50FU);
+
+    while (0U != (MC_ME->MODE_UPD & MC_ME_MODE_UPD_MODE_UPD_MASK))
+    {
     }
 }
 
--- MCXE/MCXE31B/drivers/fsl_power.h
+++ MCXE/MCXE31B/drivers/fsl_power.h
@@ -19,7 +19,9 @@
 /*! @name Driver version */
 /*! @{ */
 /*! @brief POWER driver version */
-#define FSL_POWER_DRIVER_VERSION (MAKE_VERSION(2, 0, 0)) /*!< Version 2.0.0. */
+#define FSL_POWER_DRIVER_VERSION                \
+    (MAKE_VERSION(2, 0, 0)) /*!< Version 2.0.0. \
+                             */
 /*! @} */
 
 /*! @name POWER Configuration */
@@ -28,28 +30,29 @@
 /*! @brief Power PMC voltage event flag enumeration. */
 typedef enum _pmc_event_flag
 {
-    kPMC_FPMVDD_HV_ALowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVRAF_MASK,    /*!< Low-voltage reset event on VDD_HV_A domain in FPM(full performance mode). */
+    kPMC_FPMVDD_HV_ALowPowerVoltageResetEventFlag = PMC_LVSC_LVRAF_MASK, /*!< Low-voltage reset event on VDD_HV_A domain
+                                                                            in FPM(full performance mode). */
     kPMC_LPMFPMVDD_HV_ALowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVRALPF_MASK,  /*!< Low-power reset event on VDD_HV_A domain in both LPM(low performance mode) and FPM.
-                                 */
-    kPMC_FPMVDD_HV_BLowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVRBF_MASK,    /*!< Low-voltage reset event on VDD_HV_B domain in FPM. */
-    kPMC_LPMFPMVDD_HV_BLowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVRBLPF_MASK,  /*!< Low-power reset event on VDD_HV_B domain in both LPM and FPM. */
-    kPMC_FPMV25LowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVR25F_MASK,   /*!< Low-voltage reset event on V25 domain in FPM. */
-    kPMC_LPMFPMV25LowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVR25LPF_MASK, /*!< Low-power reset event on V25 domain in both LPM and FPM. */
-    kPMC_FPMV11LowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVR11F_MASK,   /*!< Low-voltage reset event on V11 1.1V domain in FPM. */
-    kPMC_LPMFPMV11LowPowerVoltageResetEventFlag =
-        PMC_LVSC_LVR11LPF_MASK, /*!< Low-power reset event on V11 1.1V domain in both LPM and FPM. */
-    kPMC_FPMOSCV25GoNoGoEventFlag =
-        PMC_LVSC_GNG25OSCF_MASK, /*!<  Go/NoGo sensor has detected a low voltage on the V25 domain in FPM. */
-    kPMC_FPMOSCV11GoNoGoEventFlag =
-        PMC_LVSC_GNG11OSCF_MASK, /*!<  Go/NoGo sensor has detected a low voltage on the V11 domain in FPM. */
-    kPMC_POREventFlag  = PMC_LVSC_PORF_MASK, /*!< POR event flag. */
+        PMC_LVSC_LVRALPF_MASK, /*!< Low-power reset event on VDD_HV_A domain in
+                                * both LPM(low performance mode) and FPM.
+                                */
+    kPMC_FPMVDD_HV_BLowPowerVoltageResetEventFlag = PMC_LVSC_LVRBF_MASK, /*!< Low-voltage reset event on VDD_HV_B domain
+                                                                            in FPM. */
+    kPMC_LPMFPMVDD_HV_BLowPowerVoltageResetEventFlag = PMC_LVSC_LVRBLPF_MASK, /*!< Low-power reset event on VDD_HV_B
+                                                                                 domain in both LPM and FPM. */
+    kPMC_FPMV25LowPowerVoltageResetEventFlag = PMC_LVSC_LVR25F_MASK, /*!< Low-voltage reset event on V25 domain in FPM.
+                                                                      */
+    kPMC_LPMFPMV25LowPowerVoltageResetEventFlag = PMC_LVSC_LVR25LPF_MASK, /*!< Low-power reset event on V25 domain in
+                                                                             both LPM and FPM. */
+    kPMC_FPMV11LowPowerVoltageResetEventFlag = PMC_LVSC_LVR11F_MASK, /*!< Low-voltage reset event on V11 1.1V domain in
+                                                                        FPM. */
+    kPMC_LPMFPMV11LowPowerVoltageResetEventFlag = PMC_LVSC_LVR11LPF_MASK, /*!< Low-power reset event on V11 1.1V domain
+                                                                             in both LPM and FPM. */
+    kPMC_FPMOSCV25GoNoGoEventFlag = PMC_LVSC_GNG25OSCF_MASK, /*!<  Go/NoGo sensor has detected a low voltage
+                                                                on the V25 domain in FPM. */
+    kPMC_FPMOSCV11GoNoGoEventFlag = PMC_LVSC_GNG11OSCF_MASK, /*!<  Go/NoGo sensor has detected a low voltage
+                                                                on the V11 domain in FPM. */
+    kPMC_POREventFlag  = PMC_LVSC_PORF_MASK,                 /*!< POR event flag. */
     kPMC_AllEventFlags = PMC_LVSC_LVRAF_MASK | PMC_LVSC_LVRALPF_MASK | PMC_LVSC_LVRBF_MASK | PMC_LVSC_LVRBLPF_MASK |
                          PMC_LVSC_LVR25F_MASK | PMC_LVSC_LVR25LPF_MASK | PMC_LVSC_LVR11F_MASK | PMC_LVSC_LVR11LPF_MASK |
                          PMC_LVSC_GNG25OSCF_MASK | PMC_LVSC_GNG11OSCF_MASK | PMC_LVSC_PORF_MASK /*!< All event flags. */
@@ -58,20 +61,20 @@ typedef enum _pmc_event_flag
 /*! @brief List of Power PMC high and low voltage detect flags enumeration */
 typedef enum _pmc_voltage_detect_flag
 {
-    kPMC_FPMVDD_HV_AHighVoltageDetectFlag =
-        PMC_LVSC_HVDAF_MASK, /*!< Voltage on VDD_HV_A is above high-voltage detect threshold and FPM is active. */
-    kPMC_FPMVDD_HV_BHighVoltageDetectFlag =
-        PMC_LVSC_HVDBF_MASK, /*!< Voltage on VDD_HV_B is above high-voltage detect threshold and FPM is active. */
-    kPMC_FPMV25HighVoltageDetectFlag = PMC_LVSC_HVD25F_MASK,  /*!< Voltage on V25 is above high-voltage detect threshold
-                                                                 and full performance mode is active. */
-    kPMC_FPMV11HighVoltageDetectFlag = PMC_LVSC_HVD11F_MASK,  /*!< Voltage on V11 is above high-voltage detect threshold
-                                                                 and full performance mode is active. */
+    kPMC_FPMVDD_HV_AHighVoltageDetectFlag = PMC_LVSC_HVDAF_MASK, /*!< Voltage on VDD_HV_A is above high-voltage detect
+                                                                    threshold and FPM is active. */
+    kPMC_FPMVDD_HV_BHighVoltageDetectFlag = PMC_LVSC_HVDBF_MASK, /*!< Voltage on VDD_HV_B is above high-voltage detect
+                                                                    threshold and FPM is active. */
+    kPMC_FPMV25HighVoltageDetectFlag = PMC_LVSC_HVD25F_MASK,     /*!< Voltage on V25 is above high-voltage detect
+                                                                    threshold and full performance mode is active. */
+    kPMC_FPMV11HighVoltageDetectFlag = PMC_LVSC_HVD11F_MASK,     /*!< Voltage on V11 is above high-voltage detect
+                                                                    threshold and full performance mode is active. */
     kPMC_FPMAllHighVoltageDetectFlags = PMC_LVSC_HVDAF_MASK | PMC_LVSC_HVDBF_MASK | PMC_LVSC_HVD25F_MASK |
-                                        PMC_LVSC_HVD11F_MASK, /*!< All high voltage detect flags. */
-    kPMC_VDD_HV_AV5LowVoltageDetectFlag =
-        PMC_LVSC_LVD5AF_MASK,                            /*!< Voltage on VDD_HV_A is below 5V low-voltage threshold. */
-    kPMC_V15LowVoltageDetectFlag = PMC_LVSC_LVD15F_MASK, /*!< Voltage on V15 is below low-voltage detect threshold and
-                                                            full performance mode is active. */
+                                        PMC_LVSC_HVD11F_MASK,    /*!< All high voltage detect flags. */
+    kPMC_VDD_HV_AV5LowVoltageDetectFlag = PMC_LVSC_LVD5AF_MASK,  /*!< Voltage on VDD_HV_A is below 5V low-voltage
+                                                                    threshold. */
+    kPMC_V15LowVoltageDetectFlag = PMC_LVSC_LVD15F_MASK,         /*!< Voltage on V15 is below low-voltage detect
+                                                                    threshold and full performance mode is active. */
     kPMC_AllLowVoltageDetectFlags = PMC_LVSC_LVD5AF_MASK | PMC_LVSC_LVD15F_MASK, /*!< All low voltage detect flags. */
     kPMC_AllVoltageDetectFlags    = PMC_LVSC_HVDAF_MASK | PMC_LVSC_HVDBF_MASK | PMC_LVSC_HVD25F_MASK |
                                  PMC_LVSC_HVD11F_MASK | PMC_LVSC_LVD5AF_MASK |
@@ -81,18 +84,18 @@ typedef enum _pmc_voltage_detect_flag
 /*! @brief List of POWER PMC high and low voltage detect status enumeration */
 typedef enum _pmc_voltage_detect_status
 {
-    kPMC_FPMVDD_HV_AHighVoltageStatus =
-        PMC_LVSC_HVDAS_MASK,  /*!< Voltage on VDD_HV_A is above high-voltage detect threshold and FPM is active. */
-    kPMC_FPMVDD_HV_BHighVoltageStatus =
-        PMC_LVSC_HVDBS_MASK,  /*!< Voltage on VDD_HV_B is above high-voltage detect threshold and FPM is active. */
-    kPMC_FPMV25HighVoltageStatus =
-        PMC_LVSC_HVD25S_MASK, /*!< Voltage on V25 is above high-voltage detect threshold and FPM is active. */
-    kPMC_FPMV11HighVoltageStatus =
-        PMC_LVSC_HVD11S_MASK, /*!< Voltage on V11 is above high-voltage detect threshold and FPM is active. */
-    kPMC_VDD_HV_AV5LowVoltageStatus =
-        PMC_LVSC_LVD5AS_MASK, /*!< Voltage on VDD_HV_A is below low-voltage detect threshold. */
-    kPMC_V15LowVoltageStatus =
-        PMC_LVSC_LVD15S_MASK, /*!< Voltage on V15 is below low-voltage detect threshold and FPM is active. */
+    kPMC_FPMVDD_HV_AHighVoltageStatus = PMC_LVSC_HVDAS_MASK, /*!< Voltage on VDD_HV_A is above high-voltage detect
+                                                                threshold and FPM is active. */
+    kPMC_FPMVDD_HV_BHighVoltageStatus = PMC_LVSC_HVDBS_MASK, /*!< Voltage on VDD_HV_B is above high-voltage detect
+                                                                threshold and FPM is active. */
+    kPMC_FPMV25HighVoltageStatus = PMC_LVSC_HVD25S_MASK,     /*!< Voltage on V25 is above high-voltage detect
+                                                                threshold and FPM is active. */
+    kPMC_FPMV11HighVoltageStatus = PMC_LVSC_HVD11S_MASK,     /*!< Voltage on V11 is above high-voltage detect
+                                                                threshold and FPM is active. */
+    kPMC_VDD_HV_AV5LowVoltageStatus = PMC_LVSC_LVD5AS_MASK,  /*!< Voltage on VDD_HV_A is below low-voltage detect
+                                                                threshold. */
+    kPMC_V15LowVoltageStatus = PMC_LVSC_LVD15S_MASK,         /*!< Voltage on V15 is below low-voltage detect
+                                                                threshold and FPM is active. */
     kPMC_AllVoltageDetectStatus = PMC_LVSC_HVDAS_MASK | PMC_LVSC_HVDBS_MASK | PMC_LVSC_HVD25S_MASK |
                                   PMC_LVSC_HVD11S_MASK | PMC_LVSC_LVD5AS_MASK |
                                   PMC_LVSC_LVD15S_MASK /*!< All voltage detect status. */
@@ -104,7 +107,8 @@ typedef enum _pmc_interrupt_enable
     kPMC_HighVoltageDetectInterruptEnable = PMC_CONFIG_HVDIE_MASK, /*!< High voltage detect interrupt enable. */
     kPMC_LowVoltageDetectInterruptEnable  = PMC_CONFIG_LVDIE_MASK, /*!< Low voltage detect interrupt enable. */
     kPMC_AnyVoltageDetectInterruptEnable =
-        PMC_CONFIG_HVDIE_MASK | PMC_CONFIG_LVDIE_MASK,             /*!< Hign and low voltage detect interrupt enable. */
+        PMC_CONFIG_HVDIE_MASK | PMC_CONFIG_LVDIE_MASK,             /*!< Hign and low voltage detect interrupt enable.
+                                                                    */
 } pmc_interrupt_enable_t;
 
 /*!
@@ -135,7 +139,7 @@ enum _mc_me_previous_mode
 typedef enum _mc_me_reset_type
 {
     kMC_ME_DestructiveReset = MC_ME_MODE_CONF_DEST_RST_MASK, /*!< Destructive reset request */
-    kMC_ME_FunctionalReset = MC_ME_MODE_CONF_FUNC_RST_MASK /*!< Functional reset request */
+    kMC_ME_FunctionalReset  = MC_ME_MODE_CONF_FUNC_RST_MASK  /*!< Functional reset request */
 } mc_me_reset_type_t;
 
 /*! @} */
@@ -157,21 +161,22 @@ extern "C" {
 /*!
  * @brief Enable POWER PMC last mile regulator.
  *
- * @param externalBJTUsed Set to true if external BJT between VDD_HV_A and V15 is used,
- * otherwise set to false
+ * @param externalBJTUsed Set to true if external BJT between VDD_HV_A and V15
+ * is used, otherwise set to false
  */
 static inline void POWER_EnableLastMileRegulator(bool externalBJTUsed)
 {
     /*
-     * This field must be set to 1 if external BJT between VDD_HV_A and V15 is used on the PCB.
-     * The base of this BJT must be connected to the VRC_CTRL pin and is controlled by the PMC to regulate a voltage
-     * of 1.5V on V15 pin.
+     * This field must be set to 1 if external BJT between VDD_HV_A and V15 is
+     * used on the PCB. The base of this BJT must be connected to the VRC_CTRL pin
+     * and is controlled by the PMC to regulate a voltage of 1.5V on V15 pin.
      */
     if (externalBJTUsed)
     {
         PMC->CONFIG |= (uint32_t)PMC_CONFIG_LMBCTLEN_MASK;
     }
-    /* After setting LMBCTLEN=1 the software has to wait for LVD15S=0 and then setting LMEN=1 */
+    /* After setting LMBCTLEN=1 the software has to wait for LVD15S=0 and then
+     * setting LMEN=1 */
     while (PMC->LVSC & (uint32_t)PMC_LVSC_LVD15S_MASK)
     {
     }
@@ -275,8 +280,8 @@ static inline uint32_t POWER_GetPMCEventFlags(void)
 /*!
  * @brief Clear the event flag.
  *
- * @param mask    The event flags to clear. This is a logical OR of members of the
- *               enumeration ::pmc_event_flag_t
+ * @param mask    The event flags to clear. This is a logical OR of members of
+ * the enumeration ::pmc_event_flag_t
  */
 static inline void POWER_ClearPMCEventFlags(uint32_t mask)
 {
@@ -305,8 +310,8 @@ static inline uint32_t POWER_GetVoltageDetectStatus(void)
 /*!
  * @brief Enable POWER PMC voltage detect interrupt.
  *
- * @param mask    The interrupts to enable. This is a logical OR of members of the
- *                enumeration ::pmc_interrupt_enable_t
+ * @param mask    The interrupts to enable. This is a logical OR of members of
+ * the enumeration ::pmc_interrupt_enable_t
  */
 static inline void POWER_EnablePMCInterrupts(uint32_t mask)
 {
@@ -317,8 +322,8 @@ static inline void POWER_EnablePMCInterrupts(uint32_t mask)
 /*!
  * @brief Disable POWER PMC voltage detect interrupt.
  *
- * @param mask    The interrupts to disable. This is a logical OR of members of the
- *                enumeration ::pmc_interrupt_enable_t
+ * @param mask    The interrupts to disable. This is a logical OR of members of
+ * the enumeration ::pmc_interrupt_enable_t
  */
 static inline void POWER_DisablePMCInterrupts(uint32_t mask)
 {
@@ -341,8 +346,8 @@ static inline uint32_t POWER_GetPMCInterruptFlags(void)
 /*!
  * @brief Clear PMC interrupt flags.
  *
- * @param mask    The interrupts to clear. This is a logical OR of members of the
- *                enumeration ::pmc_voltage_detect_flag_t
+ * @param mask    The interrupts to clear. This is a logical OR of members of
+ * the enumeration ::pmc_voltage_detect_flag_t
  *
  */
 static inline void POWER_ClearPMCInterruptFlags(uint32_t mask)
@@ -361,7 +366,8 @@ static inline void POWER_ClearPMCInterruptFlags(uint32_t mask)
 /*!
  * @brief Gets the POWER PMC version id.
  *
- * This function gets the PMC version id including whether the PMC supports last mile regulator.
+ * This function gets the PMC version id including whether the PMC supports last
+ * mile regulator.
  *
  * @param versionId         Pointer to pmc_version_id_t structure.
  */
@@ -377,7 +383,8 @@ void POWER_GetPMCVersionID(pmc_version_id_t *versionId);
 /*!
  * @brief Check previous mode is standby mode or not.
  *
- * @return true if previous mode is standby mode, false if previous mode is reset mode.
+ * @return true if previous mode is standby mode, false if previous mode is
+ * reset mode.
  */
 static inline bool POWER_ExitFromStandbyMode(void)
 {
@@ -394,40 +401,14 @@ static inline bool POWER_ExitFromStandbyMode(void)
 /*!
  * @brief Request to enter standby mode.
  */
-static inline void POWER_EnterStandbyMode(void)
-{
-    MC_ME->MAIN_COREID = MC_ME_MAIN_COREID_CIDX(0U) | MC_ME_MAIN_COREID_PIDX(0U);
-
-    MC_ME->MODE_CONF = MC_ME_MODE_CONF_STANDBY_MASK;
-    MC_ME->MODE_UPD  = MC_ME_MODE_UPD_MODE_UPD_MASK;
-
-    /*! Write the first key */
-    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0x5AF0U);
-    /*! Write the inverted key */
-    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0xA50FU);
-
-    __ASM volatile("wfi");
-}
+void POWER_EnterStandbyMode(void);
 
 /*!
  * @brief Request destructive or functional reset mode transition.
  *
  * @param resetType Reset type, see ref mc_me_reset_type_t.
  */
-void POWER_RequestResetMode(mc_me_reset_type_t resetType)
-{
-    MC_ME->MODE_CONF = (uint32_t)resetType;
-    MC_ME->MODE_UPD = MC_ME_MODE_UPD_MODE_UPD_MASK;
-
-    /*! Write the first key */
-    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0x5AF0U);
-    /*! Write the inverted key */
-    MC_ME->CTL_KEY = MC_ME_CTL_KEY_KEY(0xA50FU);
-
-    while (0U != (MC_ME->MODE_UPD & MC_ME_MODE_UPD_MODE_UPD_MASK))
-    {
-    }
-}
+void POWER_RequestResetMode(mc_me_reset_type_t resetType);
 
 /*! @} */
 

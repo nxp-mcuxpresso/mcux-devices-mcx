1217ccfb454868176a1bea1bb28720d41d5d49ad
Michal Smola <michal.smola@nxp.com>
2025-05-06 08:42:31 +0200

    [MCUX-79564][MCXE246] Fix redefinition of MCXE246_SERIES
    MCXE246_SERIES redefinition warning occurs during a build.
    Fix the redefinition.
    
    Signed-off-by: Michal Smola <michal.smola@nxp.com>

--- MCXE/MCXE246/MCXE246_COMMON.h
+++ MCXE/MCXE246/MCXE246_COMMON.h
@@ -209,7 +209,9 @@ typedef enum IRQn {
  */ /* end of group Cortex_Core_Configuration */
 
 
+#ifndef MCXE246_SERIES
 #define MCXE246_SERIES
+#endif
 /* CPU specific feature definitions */
 #include "MCXE246_features.h"
 

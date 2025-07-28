30eb306106a233ff9e7a5a3ba603f9ff85215fae
Neil Chen <cheng.chen_1@nxp.com>
2025-07-28 13:59:02 +0800

    [MCUX-80336]Support MCXN556S max frequency to 170MHz
    Signed-off-by: Neil Chen <cheng.chen_1@nxp.com>

--- MCXN/MCXN947/drivers/fsl_clock.h
+++ MCXN/MCXN947/drivers/fsl_clock.h
@@ -51,7 +51,11 @@
 
 /* Definition for delay API in clock driver, users can redefine it to the real application. */
 #ifndef SDK_DEVICE_MAXIMUM_CPU_CLOCK_FREQUENCY
+#if defined(MCXN556S_cm33_core0_SERIES) || defined(MCXN556S_cm33_core1_SERIES)
+#define SDK_DEVICE_MAXIMUM_CPU_CLOCK_FREQUENCY (170000000UL)
+#else
 #define SDK_DEVICE_MAXIMUM_CPU_CLOCK_FREQUENCY (150000000UL)
+#endif
 #endif
 
 /*! @brief Clock ip name array for ROM. */
